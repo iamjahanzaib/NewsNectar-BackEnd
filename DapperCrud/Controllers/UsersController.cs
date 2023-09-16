83dbd056d24a2b93c7ded20c1efcf2b8f69f01ac
Jahanzaib Ikram <ikramjahanzaib@gmail.com>
2023-09-17 02:29:36 +0500

    Add GetById method in Users Controller

--- DapperCrud/Controllers/UsersController.cs
+++ DapperCrud/Controllers/UsersController.cs
@@ -24,5 +24,15 @@ namespace DapperCrud.Controllers
             var users = await connection.QueryAsync<Users>("select * from users");
             return Ok(users);
         }
+
+        [HttpGet("{userId}")]
+
+        public async Task<ActionResult<Users>> GetUser(int userId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var user = await connection.QueryFirstAsync<Users>("select * from users where id = @Id",
+                new {Id = userId});
+            return Ok(user);
+        }
     }
 }
