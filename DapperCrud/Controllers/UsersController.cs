41c3598507be6d9f2e38e3c0bb77e8c4921ec1a7
Jahanzaib Ikram <ikramjahanzaib@gmail.com>
2023-09-17 02:15:27 +0500

    Add project files.

---
+++ DapperCrud/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace DapperCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public UsersController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<List<Users>>> GetAllUsers()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var users = await connection.QueryAsync<Users>("select * from users");
+            return Ok(users);
+        }
+    }
+}
---
+++ DapperCrud/Users.cs
@@ -0,0 +1,11 @@
+﻿namespace DapperCrud
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string LatName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+
+    }
+}
