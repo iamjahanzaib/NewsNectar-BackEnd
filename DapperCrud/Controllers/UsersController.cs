e608eda91d6452be7a6ef864bbfa41cf4bfe1085
Jahanzaib Ikram <ikramjahanzaib@gmail.com>
2023-09-18 12:00:49 +0500

    Fixed not including Id in post

--- DapperCrud/Controllers/UsersController.cs
+++ DapperCrud/Controllers/UsersController.cs
@@ -21,7 +21,7 @@ namespace DapperCrud.Controllers
         public async Task<ActionResult<List<Users>>> GetAllUsers()
         {
             using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
-            var users = await connection.QueryAsync<Users>("select * from users");
+            IEnumerable<Users> users = await GetAllUsers(connection);
             return Ok(users);
         }
 
@@ -33,6 +33,22 @@ namespace DapperCrud.Controllers
             var user = await connection.QueryFirstAsync<Users>("select * from users where id = @Id",
                 new {Id = userId});
             return Ok(user);
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<List<Users>>> AddUser(Users user)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("insert into users (id, name, lastName, address) values (@Id, @Name, @LastName, @Address)", user);
+            return Ok(await GetAllUsers(connection));
+        }
+
+
+
+        private static async Task<IEnumerable<Users>> GetAllUsers(SqlConnection connection)
+        {
+            return await connection.QueryAsync<Users>("select * from users");
         }
     }
 }
--- DapperCrud/Users.cs
+++ DapperCrud/Users.cs
@@ -2,9 +2,9 @@
 {
     public class Users
     {
-        public int Id { get; set; }
+        public int Id { get; } 
         public string Name { get; set; } = string.Empty;
-        public string LatName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
 
     }
